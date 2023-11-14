78d48ea9feae8b061ed17ef64376ec1e087d5795
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-11-14 15:43:55 +0900

    ボトムナビゲーションバーのUIを作成。

--- app/src/main/java/com/example/musiceducationtest/MainActivity.kt
+++ app/src/main/java/com/example/musiceducationtest/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.musiceducationtest
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.support.v4.os.IResultReceiver.Default
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -118,10 +121,71 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
                         }
+
+                        // ボトムナビゲーションバー
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .align(Alignment.BottomCenter)
+                                    .background(Color(0xFFEEEEEE)),
+                            ) {
+                                Row {
+                                    BottomNavigateButton(
+                                        imageVector = Icons.Default.Clear,
+                                        label = "やめる",
+                                        backgroundColor = Color(0xFF424242)
+                                    )
+                                    Spacer(modifier = Modifier.weight(1f))
+                                    BottomNavigateButton(
+                                        imageVector = Icons.Default.KeyboardArrowLeft,
+                                        label = "もどる",
+                                        backgroundColor = Color(0xFF26A69A)
+                                    )
+                                    BottomNavigateButton(
+                                        imageVector = Icons.Default.KeyboardArrowRight,
+                                        label = "つぎへ",
+                                        backgroundColor = Color(0xFFFF7043)
+                                    )
+                                }
+                            }
+                        }
                     }
 
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigateButton(
+    imageVector: ImageVector,
+    label: String,
+    backgroundColor: Color
+) {
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(backgroundColor)
+            .clickable { /* クリックされた時の処理*/ },
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = "",
+                tint = Color.White,
+                modifier = Modifier.size(60.dp)
+            )
+            Text(
+                text = label,
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
