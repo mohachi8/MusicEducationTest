84f96bd815b1b2b9f0a6f036fa92637079bcaf2a
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-11-15 12:13:43 +0900

    MainContentに画面構成を移行

--- app/src/main/java/com/example/musiceducationtest/MainActivity.kt
+++ app/src/main/java/com/example/musiceducationtest/MainActivity.kt
@@ -41,168 +41,7 @@ class MainActivity : ComponentActivity() {
                         bottomBar = { BottomNavigationBar() }
                     ) { innerPadding ->
                         Box(modifier = Modifier.padding(innerPadding)) {
-                            Column {
-                                // フローチャート表示エリア
-                                Row(
-                                    modifier = Modifier.padding(20.dp),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    // 曲の再生ボタン
-                                    Button(
-                                        onClick = { /*TODO*/ },
-                                        colors = ButtonDefaults.buttonColors(Color(0xFFFF7043))
-                                    ) {
-                                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                                            Icon(
-                                                imageVector = Icons.Default.PlayArrow,
-                                                contentDescription = "再生",
-                                                modifier = Modifier.size(40.dp),
-                                                tint = Color.White
-                                            )
-                                            Spacer(modifier = Modifier.height(2.dp))
-                                            Text(
-                                                text = "曲を再生",
-                                                fontSize = 16.sp,
-                                                fontWeight = FontWeight.Bold,
-                                                color = Color.White
-                                            )
-                                        }
-                                    }
-
-                                    Spacer(modifier = Modifier.width(20.dp))
-
-                                    // フローチャート
-                                    LazyRow(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .background((Color(0xFFEEEEEE)))
-                                            .border(width = 4.dp, color = Color.Black)
-                                            .padding(20.dp),
-                                        verticalAlignment = Alignment.CenterVertically
-                                    ) {
-                                        item {
-                                            Box(
-                                                modifier = Modifier
-                                                    .size(100.dp)
-                                                    .background(Color.Blue),
-                                                contentAlignment = Alignment.Center
-                                            ) {
-                                                Text(
-                                                    text = "スタート",
-                                                    color = Color.White,
-                                                    fontSize = 16.sp,
-                                                    fontWeight = FontWeight.Bold
-                                                )
-                                            }
-                                            Spacer(modifier = Modifier.width(5.dp))
-                                            Icon(
-                                                imageVector = Icons.Default.PlayArrow,
-                                                contentDescription = "次へ",
-                                                modifier = Modifier.size(40.dp)
-                                            )
-                                            Spacer(modifier = Modifier.width(5.dp))
-                                        }
-                                        items(10) { index ->
-                                            Box(
-                                                modifier = Modifier
-                                                    .size(100.dp)
-                                                    .background(Color.LightGray)
-                                            )
-                                            Spacer(modifier = Modifier.width(5.dp))
-                                            Icon(
-                                                imageVector = Icons.Default.PlayArrow,
-                                                contentDescription = "次へ",
-                                                modifier = Modifier.size(40.dp)
-                                            )
-                                            Spacer(modifier = Modifier.width(5.dp))
-                                        }
-                                        item {
-                                            Box(
-                                                modifier = Modifier
-                                                    .size(100.dp)
-                                                    .background(Color.LightGray)
-                                            )
-                                        }
-                                    }
-                                }
-
-                                // 選択肢エリア
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .background((Color(0xFFEEEEEE)))
-                                        .border(width = 4.dp, color = Color.Black)
-                                        .padding(20.dp),
-                                ) {
-                                    // 選択肢操作ボタンエリア
-                                    Row(
-                                        verticalAlignment = Alignment.CenterVertically
-                                    ) {
-                                        Text(
-                                            text = "ブロック エリア",
-                                            fontSize = 20.sp,
-                                            fontWeight = FontWeight.ExtraBold,
-                                        )
-                                        Spacer(modifier = Modifier.width(20.dp))
-                                        Button(onClick = { /*TODO*/ }) {
-                                            Row(
-                                                verticalAlignment = Alignment.CenterVertically,
-                                                modifier = Modifier.padding(5.dp)
-                                            ) {
-                                                Text(
-                                                    text = "えらぶ",
-                                                    fontSize = 16.sp,
-                                                    fontWeight = FontWeight.Bold
-                                                )
-                                                Spacer(modifier = Modifier.width(10.dp))
-                                                androidx.compose.material.Icon(
-                                                    imageVector = Icons.Default.Check,
-                                                    contentDescription = "えらぶ"
-                                                )
-                                            }
-                                        }
-                                        Spacer(modifier = Modifier.width(20.dp))
-                                        Button(onClick = { /*TODO*/ }) {
-                                            Row(
-                                                verticalAlignment = Alignment.CenterVertically,
-                                                modifier = Modifier.padding(5.dp)
-                                            ) {
-                                                Text(
-                                                    text = "けす",
-                                                    fontSize = 16.sp,
-                                                    fontWeight = FontWeight.Bold
-                                                )
-                                                Spacer(modifier = Modifier.width(10.dp))
-                                                androidx.compose.material.Icon(
-                                                    imageVector = Icons.Default.Delete,
-                                                    contentDescription = "けす"
-                                                )
-                                            }
-                                        }
-                                    }
-                                    Spacer(modifier = Modifier.height(20.dp))
-
-                                    LazyHorizontalGrid(
-                                        rows = GridCells.Adaptive(minSize = 100.dp),
-                                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                                        horizontalArrangement = Arrangement.spacedBy(16.dp),
-                                        modifier = Modifier
-                                            .background(Color.White)
-                                            .padding(20.dp)
-                                            .heightIn(min = 100.dp, max = 300.dp) // 最小高さ100dp、最大高さ300dp
-                                            .fillMaxWidth()
-                                    ) {
-                                        items(30) { index ->
-                                            Box(
-                                                modifier = Modifier
-                                                    .size(100.dp)
-                                                    .background(Color.LightGray)
-                                                    .clickable {  }
-                                            )
-                                        }
-                                    }
-                                }
-                            }
+                            MainContent()
                         }
                     }
                 }
@@ -254,7 +93,7 @@ fun BottomNavigationBar() {
     }
 }
 
-
+// ボトムナビゲーションバーボタン
 @Composable
 fun BottomNavigateButton(
     imageVector: ImageVector,
@@ -283,6 +122,172 @@ fun BottomNavigateButton(
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold
             )
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Column {
+        // フローチャート表示エリア
+        Row(
+            modifier = Modifier.padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // 曲の再生ボタン
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color(0xFFFF7043))
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = "再生",
+                        modifier = Modifier.size(40.dp),
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text(
+                        text = "曲を再生",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            // フローチャート
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background((Color(0xFFEEEEEE)))
+                    .border(width = 4.dp, color = Color.Black)
+                    .padding(20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                item {
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .background(Color.Blue),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "スタート",
+                            color = Color.White,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = "次へ",
+                        modifier = Modifier.size(40.dp)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                }
+                items(10) { index ->
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .background(Color.LightGray)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = "次へ",
+                        modifier = Modifier.size(40.dp)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                }
+                item {
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .background(Color.LightGray)
+                    )
+                }
+            }
+        }
+
+        // 選択肢エリア
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background((Color(0xFFEEEEEE)))
+                .border(width = 4.dp, color = Color.Black)
+                .padding(20.dp),
+        ) {
+            // 選択肢操作ボタンエリア
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "ブロック エリア",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+                        Text(
+                            text = "えらぶ",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        androidx.compose.material.Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "えらぶ"
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+                        Text(
+                            text = "けす",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        androidx.compose.material.Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = "けす"
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LazyHorizontalGrid(
+                rows = GridCells.Adaptive(minSize = 100.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(20.dp)
+                    .heightIn(min = 100.dp, max = 300.dp) // 最小高さ100dp、最大高さ300dp
+                    .fillMaxWidth()
+            ) {
+                items(30) { index ->
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .background(Color.LightGray)
+                            .clickable { }
+                    )
+                }
+            }
         }
     }
 }
