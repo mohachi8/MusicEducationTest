d5c6a46a5a2a17ea55253c86d2780b513b957557
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-11-21 20:06:03 +0900

    ExplanationScreenを追加

---
+++ app/src/main/java/com/example/musiceducationtest/ExplanationScreen.kt
@@ -0,0 +1,9 @@
+package com.example.musiceducationtest
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExplanationScreen() {
+    Text(text = "下の音楽プレイヤーで音楽を聞きましょう。")
+}
