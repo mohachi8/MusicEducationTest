97cea54ef1feacfcc9a3ba5f9954c74fc690d1c3
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-11-22 17:25:18 +0900

    問題選択画面で曲の題名が表示される機能を実装

--- app/src/main/java/com/example/musiceducationtest/QuestionSelectionScreen.kt
+++ app/src/main/java/com/example/musiceducationtest/QuestionSelectionScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +21,11 @@ import com.example.musiceducationtest.ui.theme.Purple200
 @Composable
 fun QuestionSelectionScreen(navController: NavController) {
 
-    val lessons = listOf("レッスン1", "レッスン2", "レッスン3")
+    val lessons = listOf(
+        Lesson("レッスン1", "きらきら星"),
+        Lesson("レッスン2", "うみはひろいな"),
+        Lesson("レッスン3", "かっこう")
+    )
 
     LazyVerticalGrid(
         columns = GridCells.Adaptive(300.dp),
@@ -28,15 +33,18 @@ fun QuestionSelectionScreen(navController: NavController) {
         modifier = Modifier
             .padding(start = 100.dp, end = 100.dp, top = 80.dp)
     ) {
-        items(lessons.size) { index ->
-            val lesson = lessons[index]
-            LessonBox(lesson = lesson, lessonId = index, navController = navController)
+        items(lessons) { lesson ->
+            LessonBox(
+                lesson = lesson,
+                lessonId = lessons.indexOf(lesson),
+                navController = navController
+            )
         }
     }
 }
 
 @Composable
-fun LessonBox(lesson: String, lessonId: Int, navController: NavController) {
+fun LessonBox(lesson: Lesson, lessonId: Int, navController: NavController) {
     Box(
         modifier = Modifier
             .padding(8.dp)
@@ -45,21 +53,27 @@ fun LessonBox(lesson: String, lessonId: Int, navController: NavController) {
             .background(Purple200)
             .clickable { navController.navigate("explanation/$lessonId") }
             .border(width = 8.dp, color = Color.Black),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center,
     ) {
-        Column(verticalArrangement = Arrangement.Center) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
-                text = lesson,
+                text = lesson.title,
                 fontWeight = FontWeight.Bold,
                 fontSize = 20.sp,
                 color = Color.White
             )
             Spacer(modifier = Modifier.height(20.dp))
             Text(
-                text = "きらきら星",
+                text = lesson.description,
                 fontSize = 16.sp,
                 color = Color.White
             )
         }
     }
 }
+
+// レッスンのデータクラス
+data class Lesson(val title: String, val description: String)
