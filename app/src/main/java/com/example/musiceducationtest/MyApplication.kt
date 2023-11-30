b8666e49b027ce9aed6272d9be1b6ac3250fffb3
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-11-30 16:29:34 +0900

    Dagger　Hiltの導入（importのみ）

---
+++ app/src/main/java/com/example/musiceducationtest/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.example.musiceducationtest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    // アプリケーションの初期化ロジック
+}
--- app/src/main/java/com/example/musiceducationtest/ui/screens/LessonSelectionScreen.kt
+++ app/src/main/java/com/example/musiceducationtest/ui/screens/LessonSelectionScreen.kt
@@ -1,27 +1,17 @@
 package com.example.musiceducationtest.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.example.musiceducationtest.model.LessonDataModel
 import com.example.musiceducationtest.ui.components.LessonBox
-import com.example.musiceducationtest.ui.theme.Purple200
 import com.example.musiceducationtest.viewmodel.LessonManagerViewModel
 
 @Composable
