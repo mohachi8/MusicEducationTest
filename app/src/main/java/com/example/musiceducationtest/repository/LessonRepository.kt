6bc651b2f477a1b36b1d184657183d0863320538
mohachi8 <92675372+mohachi8@users.noreply.github.com>
2023-12-14 02:59:14 +0900

    データの中身を更新

--- app/src/main/java/com/example/musiceducationtest/repository/LessonRepository.kt
+++ app/src/main/java/com/example/musiceducationtest/repository/LessonRepository.kt
@@ -10,6 +10,41 @@ import javax.inject.Singleton
 class LessonRepository @Inject constructor() {
     private val lessons = listOf(
         LessonDataModel(
+            id = "0",
+            title = "レッスン0",
+            songTitle = "きらきら星（開発用）",
+            description = "このレッスンでは「きらきら星」を題材にした学習を行います。",
+            musicResId = R.raw.twinkle_twinkle_little_star,
+            firstFlowChartBlock = BlockDataModel.BLOCK01T_T,
+            flowChartBlocks = listOf(
+                BlockDataModel.BLOCK02T_T,
+                BlockDataModel.BLOCK03T_T,
+                BlockDataModel.BLOCK04T_T,
+                BlockDataModel.BLOCK05T_T,
+                BlockDataModel.BLOCK06T_T,
+                BlockDataModel.BLOCK07T_T,
+                BlockDataModel.BLOCK08T_T,
+                BlockDataModel.BLOCK09T_T,
+                BlockDataModel.BLOCK10T_T,
+                BlockDataModel.BLOCK11T_T,
+                BlockDataModel.BLOCK12T_T
+            ),
+            answers = listOf(
+                BlockDataModel.BLOCK01T_T,
+                BlockDataModel.BLOCK02T_T,
+                BlockDataModel.BLOCK03T_T,
+                BlockDataModel.BLOCK04T_T,
+                BlockDataModel.BLOCK05T_T,
+                BlockDataModel.BLOCK06T_T,
+                BlockDataModel.BLOCK07T_T,
+                BlockDataModel.BLOCK08T_T,
+                BlockDataModel.BLOCK09T_T,
+                BlockDataModel.BLOCK10T_T,
+                BlockDataModel.BLOCK11T_T,
+                BlockDataModel.BLOCK12T_T
+            )
+        ),
+        LessonDataModel(
             id = "1",
             title = "レッスン1",
             songTitle = "きらきら星",
